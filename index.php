8344aff44555b397208630f6956981f3b4d2ed6d
pimentelfl <pimentel.fl@gmail.com>
2021-02-04 15:36:42 -0300

    html

--- index.php
+++ index.php
@@ -4,15 +4,24 @@
 
 
 <div style="display:flex">
-	<div style="width:200px;height:200px;background:yellow;padding:50px;margin:10px">
+	<div style="width:200px;height:400px;background:yellow;padding:50px;margin:10px">
     Um teste em quadrado
     <br>
     <img src="b1.jpg" style="width: 200px">
-	</div>
+	<br><br>
+    R$ 500,00
+    <br><br>
+    <a href="https://pag.ae/7WRynf6X1" target="_blank" style="display:block;background:green;color:white;text-align:center;padding:20px;border-radium:10px">Comprar agora</a><br>    
+    <a href="https://api.whatsapp.com/send?phone=55999999999999&text=ol%C3%A1%20vendedor%20quero%20comprar%20de%20vc" target="_blank" >Entrar em contato com o vendedor</a>
+    </div>
 
-	<div style="width:200px;height:200px;background:blue;color:white;padding:50px;margin:10px">
+	<div style="width:200px;height:400px;background:blue;color:white;padding:50px;margin:10px">
     Um teste em quadrado
-    <br>
+    <br><br>
     <img src="b2.jpg" style="width: 200px">
-	</div>
+	<br><br>
+    R$ 800,00
+    <a href="https://pag.ae/7WRynf6X1" target="_blank" style="display:block;background:green;color:white;text-align:center;padding:20px;border-radium:10px">Comprar agora</a><br>    
+    <a href="https://api.whatsapp.com/send?phone=55999999999999&text=ol%C3%A1%20vendedor%20quero%20comprar%20de%20vc" target="_blank" >Entrar em contato com o vendedor</a>
+    </div>
 </div>
