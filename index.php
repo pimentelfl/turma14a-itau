eb65eeef3697d3745940620a649fef0f5dc3b675
pimentelfl <pimentel.fl@gmail.com>
2021-02-04 15:38:59 -0300

    html

--- index.php
+++ index.php
@@ -21,6 +21,7 @@
     <img src="b2.jpg" style="width: 200px">
 	<br><br>
     R$ 800,00
+    <br><br>
     <a href="https://pag.ae/7WRynf6X1" target="_blank" style="display:block;background:green;color:white;text-align:center;padding:20px;border-radium:10px">Comprar agora</a><br>    
     <a href="https://api.whatsapp.com/send?phone=55999999999999&text=ol%C3%A1%20vendedor%20quero%20comprar%20de%20vc" target="_blank" >Entrar em contato com o vendedor</a>
     </div>
