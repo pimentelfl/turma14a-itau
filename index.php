771177e3487640b77f618d65c9182f48362a7b9a
pimentelfl <pimentel.fl@gmail.com>
2021-02-04 14:55:47 -0300

    html

--- index.php
+++ index.php
@@ -1,16 +1,14 @@
 <h1>Bem vindo turma 14A do itaú pele treinamento da Gama </h1>
 
-<h3> Um deploy aotomático feito com alunos do itaú </h3>
+<h3> Um deploy automático feito com alunos do itaú </h3>
 
-<h5>Tentar sempre, desistir jamais!!!<h5>
-  
-<h7>testando!!!<h7>
-<h10>Tentar sempre, desistir as vezes!!!<h10>
-<h7>QUERO DELETAR TUDO </h7>
 
-<div style="width:200px;height:200px;background:yellow">
-    Um testes em quadrado
-</div>
-<div style="width:200px;height:200px;background:blue">
-    Dois testes em quadrado
+<div style="display:flex">
+	<div style="width:200px;height:200px;background:yellow;padding:50px;margin:10px">
+    Um teste em quadrado
+	</div>
+
+	<div style="width:200px;height:200px;background:blue;color:white;padding:50px;margin:10px">
+    Um teste em quadrado
+	</div>
 </div>
