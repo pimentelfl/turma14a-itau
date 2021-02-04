e5355bd0e71ec27238fa03acbec80881053024ec
pimentelfl <pimentel.fl@gmail.com>
2021-02-04 15:12:18 -0300

    html

--- index.php
+++ index.php
@@ -6,9 +6,13 @@
 <div style="display:flex">
 	<div style="width:200px;height:200px;background:yellow;padding:50px;margin:10px">
     Um teste em quadrado
+    <br>
+    <img src="b1.jpg" style="widht: 100px">
 	</div>
 
 	<div style="width:200px;height:200px;background:blue;color:white;padding:50px;margin:10px">
     Um teste em quadrado
+    <br>
+    <img src="b1.jpg" style="widht: 200px">
 	</div>
 </div>
