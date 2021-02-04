c62976fddadff5179661688973fbed32ccdf0edf
pimentelfl <pimentel.fl@gmail.com>
2021-02-04 15:18:57 -0300

    html

--- index.php
+++ index.php
@@ -7,12 +7,12 @@
 	<div style="width:200px;height:200px;background:yellow;padding:50px;margin:10px">
     Um teste em quadrado
     <br>
-    <img src="b1.jpg" style="widht: 100px">
+    <img src="b1.jpg" style="width: 200px">
 	</div>
 
 	<div style="width:200px;height:200px;background:blue;color:white;padding:50px;margin:10px">
     Um teste em quadrado
     <br>
-    <img src="b2.jpg" style="widht: 100px">
+    <img src="b2.jpg" style="width: 200px">
 	</div>
 </div>
